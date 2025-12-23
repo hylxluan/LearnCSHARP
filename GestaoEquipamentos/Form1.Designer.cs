cad13cf4df034b96b3fbd7a5860338fef886157f
hylxluan <luanplays212004@gmail.com>
2025-12-23 17:35:09 -0300

    Tela de Gestão de Clientes

---
+++ GestaoEquipamentos/Form1.Designer.cs
@@ -0,0 +1,87 @@
+﻿namespace GestaoClientes
+{
+    partial class frmHome
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmHome));
+            panel1 = new Panel();
+            logo = new PictureBox();
+            panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)logo).BeginInit();
+            SuspendLayout();
+            // 
+            // panel1
+            // 
+            panel1.AutoScroll = true;
+            panel1.BackColor = Color.Black;
+            panel1.Controls.Add(logo);
+            panel1.Dock = DockStyle.Left;
+            panel1.Location = new Point(0, 0);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(185, 561);
+            panel1.TabIndex = 0;
+            panel1.Paint += panel1_Paint;
+            // 
+            // logo
+            // 
+            logo.Dock = DockStyle.Top;
+            logo.Image = (Image)resources.GetObject("logo.Image");
+            logo.Location = new Point(0, 0);
+            logo.Name = "logo";
+            logo.Size = new Size(185, 102);
+            logo.SizeMode = PictureBoxSizeMode.StretchImage;
+            logo.TabIndex = 0;
+            logo.TabStop = false;
+            // 
+            // frmHome
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            AutoSize = true;
+            BackColor = Color.White;
+            ClientSize = new Size(898, 561);
+            Controls.Add(panel1);
+            FormBorderStyle = FormBorderStyle.FixedSingle;
+            Icon = (Icon)resources.GetObject("$this.Icon");
+            ImeMode = ImeMode.On;
+            MaximizeBox = false;
+            Name = "frmHome";
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "GESTÃO DE CLIENTES";
+            Load += Form1_Load;
+            panel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)logo).EndInit();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private Panel panel1;
+        private PictureBox logo;
+    }
+}
---
+++ GestaoEquipamentos/Form1.cs
@@ -0,0 +1,20 @@
+namespace GestaoClientes
+{
+    public partial class frmHome : Form
+    {
+        public frmHome()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+    }
+}
